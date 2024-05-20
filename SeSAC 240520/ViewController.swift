ab26633f9716e3df021c349fa3f619255069c4ef
binnyy <beanbaebae@naver.com>
2024-05-20 09:42:58 +0900

    Initial Commit

---
+++ SeSAC 240520/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC 240520
+//
+//  Created by 배현빈 on 5/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
