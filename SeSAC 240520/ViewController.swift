f3025dd349af40249cb226aa2242deacbf4f38e2
binnyy <beanbaebae@naver.com>
2024-05-20 11:47:09 +0900

    uiview tag 활용해서 코드 간단하게 만들기 / @IBA 여러 버튼에 연결

--- SeSAC 240520/ViewController.swift
+++ SeSAC 240520/ViewController.swift
@@ -19,24 +19,71 @@ class ViewController: UIViewController {
     @IBOutlet var threeLabel: UILabel!
     
     
+    var oneCount = 0
+    var twoCount = 1
+    var threeCount = 2
+    
+    
+    var count = [0,0,0]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // labelName : 매개변수 (Parameter)
         // oneLabel, twoLabel, threeLabel : 전달인자(Argument) _ 데이터를 실어 보내는것 (전달데이터) (ex. red)
-        designLabelUI(labelName: oneLabel, thisIsTextColor: .red)
-        designLabelUI(labelName: twoLabel, thisIsTextColor: .blue)
-        designLabelUI(labelName: threeLabel, thisIsTextColor: .green)
+       // designLabelUI(labelName: oneLabel, thisIsTextColor: .red)
+        //designLabelUI(labelName: twoLabel, thisIsTextColor: .blue)
+        //designLabelUI(labelName: threeLabel, thisIsTextColor: .green)
         
-        designButtonUI(oneButton, titleColor: .read, title: "234")
+      //  designButtonUI(oneButton, titleColor: .read, title: "234")
         
         // () 함수호출 연산자
         
     }
     
+    
+    // 1. 어떤 버튼을 클릭했는지 우찌 알죠?
+    // -> currentTitle (optional 조심, 버전 조심)
+    // -> tag
+    // 2. 아웃렛 머 어떻게 못할까 => OutletCollection
+    
     @IBAction func oneButtonClicked(_ sender: UIButton) {
-        oneLabel.text = "4567"
+
+        
+        
+        //print(sender.currentTitle)
+        print(sender.tag)
+        
+        if sender.tag == 0 {
+            count[sender.tag] = count[sender.tag] + 1
+        } else if sender.tag == 1 {
+            count[sender.tag] = count [sender.tag] +1
+        } else {
+            count[2] = count [2] + 1
+        }
+        
+        count[sender.tag] = count[sender.tag] + 1
+        
+        
+        print(count)
+        
+        oneLabel.text = "\(count[0])번 클릭"
+        twoLabel.text = "\(count[1])번 클릭"
+        threeLabel.text = "\(count[2])번 클릭"
+        
+        
+        if sender.currentTitle! == "hyunbin" {
+        // 왜 옵셔널인가? 글자 없이 이미지로만 이루어져 있을 수도 있기 때문
+        oneLabel.text = "\(oneCount)번"
+        oneCount = oneCount + 1
+        
+    } else {
+        twoLabel.text = "\(twoCount)번"
+        twoCount = twoCount + 1
     }
+   
+  
     
     
     // 매개변수 (parameter)
@@ -59,8 +106,8 @@ class ViewController: UIViewController {
     
     func designButtonUI (_ button : UIButton, titleColor better : UIColor, title: String) {
         
-        button.setTitleColor(titleColor, for: .normal)
-        better.title(title, String)
+        //button.setTitleColor(titleColor, for: .normal)
+        //better.title(title, String)
         
         //threeButton.setTitleColor(.red, for: .normal)
         //threeButton.setTitle("클릭", for: .normal)
